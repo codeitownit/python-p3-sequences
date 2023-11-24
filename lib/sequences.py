b2660b977759d20edfa4b1f84f08b6a976312e9a
Everlyn <gachuieve32@gmail.com>
2023-11-24 10:48:24 +0300

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonnaci_list = []
+    if length > 0:
+        fibonnaci_list.append(0)
+        if length > 1:
+            fibonnaci_list.append(1)
+            if length > 2:
+                for i in range(2, length):
+                    fibonnaci_list.append(
+                        fibonnaci_list[i - 1] + fibonnaci_list[i - 2])
+    print(fibonnaci_list)
+
+print_fibonacci(2)
